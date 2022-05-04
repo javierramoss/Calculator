6d667b53c16f44b1f744f78430adba9fddbc79c6
BigBeakPenguin <javierramoss@usf.edu>
2022-05-04 14:56:52 -0400

    Implement remaining and fix current arithmetic operations

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,6 @@
-#include "calculator.hpp"
+﻿#include "calculator.hpp"
 
-std::ostream& operator<<(std::ostream& out, const Calculator& calculator) {
+std::ostream& operator<<(std::ostream& out, Calculator& calculator) {
     out << calculator.getAnswer();
     return out;
 }
@@ -15,15 +15,30 @@ std::string Calculator::toString(const char& ch) {
     return temp;
 }
 
+std::string Calculator::trimTrailingZeros(std::string str) {
+    if (str.find('.') != std::string::npos)
+    {
+        str = str.substr(0, str.find_last_not_of('0') + 1);
+        if (str.find('.') == str.size() - 1)
+            str = str.substr(0, str.size() - 1);
+    }
+    return str;
+}
+
+void Calculator::AC() {
+    postfix.empty();
+    instructions.empty();
+}
+
 void Calculator::getPostfixNotation(const std::string& infix) {
     int i = 0;
     LinkedStack<char> infixOperators;
     while (i < infix.size()) {
         char ch = infix[i];
 
-        if (std::isdigit(ch)) {
+        if (std::isdigit(ch) || ch == '.') {
             std::string temp = toString(ch);
-            while (i + 1 < infix.size() && isdigit(infix[i + 1]))
+            while (i + 1 < infix.size() && (isdigit(infix[i + 1]) || infix[i + 1] == '.'))
                 temp += infix[++i];
 
             postfix.push(temp);
@@ -58,32 +73,32 @@ void Calculator::evaluate() {
     while (!postfix.empty()) {
         std::string str = postfix.top();
         if (!std::isdigit(str[0])) {
-            int n1 = instructions.top();
+            double n1 = instructions.top();
             instructions.pop();
-            int n2 = instructions.top();
+            double n2 = instructions.top();
             instructions.pop();
 
             if (str[0] == '+')
                 instructions.push(n2 + n1);
             else if (str[0] == '-')
                 instructions.push(n2 - n1);
-            else if (str[0] == 'x' || str[0] == '*')
+            else if (str[0] == 'x' || str[1] == '*')
                 instructions.push(n2 * n1);
             else if (str[0] == '/')
                 instructions.push(n2 / n1);
             else if (str[0] == '^')
                 instructions.push(pow(n2, n1));
-            else if (str[0] == 's')
+            else if (str[0] == 'r')
                 instructions.push(pow(n1, 1.0 / n2));
         }
         else {
-            instructions.push(std::stoi(str));
+            instructions.push(std::stod(trimTrailingZeros(str)));
         }
 
         postfix.pop();
     }
 }
 
-int Calculator::getAnswer() const {
-    return instructions.top();
+std::string Calculator::getAnswer() {
+    return trimTrailingZeros(std::to_string(instructions.top()));
 }
