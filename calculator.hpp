b190148e75424091dad3b87a3effd77ee7442a0e
BigBeakPenguin <javierramoss@usf.edu>
2022-05-04 14:53:14 -0400

    Declare additional helper functions

--- calculator.hpp
+++ calculator.hpp
@@ -3,23 +3,25 @@
 
 #include <string>
 #include <unordered_map>
-#include "./lib/LinkedLib/LinkedQueue.hpp"
-#include "./lib/LinkedLib/LinkedStack.hpp"
+#include "./lib/LinkedLib/linkedqueue.hpp"
+#include "./lib/LinkedLib/linkedstack.hpp"
 
 class Calculator {
-    friend std::ostream& operator<<(std::ostream& out, const Calculator& calculator);
+    friend std::ostream& operator<<(std::ostream& out, Calculator& calculator);
     friend void operator<<(Calculator& calculator, const std::string& str);
 
 private:
     LinkedQueue<std::string> postfix;
-    LinkedStack<int> instructions;
+    LinkedStack<double> instructions;
     std::unordered_map<char, int> precedence = { {'+', 1}, {'-', 1}, {'x', 2}, {'*', 2}, {'/', 2}, {'^', 3}, {'s', 3} };
 
 public:
     std::string toString(const char& ch);
+    std::string trimTrailingZeros(std::string str);
+    void AC();
     void getPostfixNotation(const std::string& infix);
     void evaluate();
-    int getAnswer() const;
+    std::string getAnswer();
 };
 
 #endif
